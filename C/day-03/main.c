e133e33e400ad7bb77a648d89e4a275d48f3462e
AngeloPavone <pavone@gmx.us>
2023-11-05 23:40:14 -0800

    idk

--- C/day-03/main.c
+++ C/day-03/main.c
@@ -4,8 +4,10 @@
 #include <errno.h>
 #include <string.h>
 
-#define SUCCESS        0
-#define FILE_NOT_FOUND NULL
+#define SUCCESS               0
+#define FILE_NOT_FOUND        1
+#define EOF_REACHED           0
+#define COULD_NOT_READ_CHUNKS 2
 
 const char* INPUT_FILE = "../../Input/2022-day03";
 char CHUNK[256];
@@ -16,18 +18,23 @@ FILE* openFile(const char* INPUT_FILE)
 
     if (fptr == NULL)
     {
-        perror("ERROR: file not found");
-        return FILE_NOT_FOUND;
+        perror("ERROR: Input file not found");
+        exit(FILE_NOT_FOUND);
     }
     return fptr;
 }
 
-void readLines(FILE* fptr)
+char* part1(FILE* fptr)
 {
-    while (fgets(CHUNK, sizeof(CHUNK), fptr) != NULL)
+    if (fgets(CHUNK, sizeof(CHUNK), fptr) != NULL)
     {
-        printf("%s", CHUNK);
+        return CHUNK;
     }
+    else
+    {
+        return EOF_REACHED;
+    }
+    exit(COULD_NOT_READ_CHUNKS);
 }
 
 int main(void)
@@ -35,9 +42,12 @@ int main(void)
 
     FILE* fptr = openFile(INPUT_FILE);
 
-    readLines(fptr);
+    while (part1(fptr) != EOF_REACHED)
+    {
+        // TODO(angelo): implement part 1 :/
+    }
 
     fclose(fptr);
 
-    return 0;
+    return SUCCESS;
 };
