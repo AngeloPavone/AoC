47227dbdb28b05fb731469548419467eeb87f0b4
AngeloPavone <pavone@gmx.us>
2023-11-06 18:23:35 -0800

    solved it

--- C/day-03/main.c
+++ C/day-03/main.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define SUCCESS               0
-#define FILE_NOT_FOUND        1
-#define EOF_REACHED           0
-#define COULD_NOT_READ_CHUNKS 2
+#define SUCCESS                     0
+#define FILE_NOT_FOUND              1
+#define EOF_REACHED                 0
+#define COULD_NOT_READ_CHUNKS       2
+#define COULD_NOT_FIND_SIMILAR_ITEM 3
 
 const char* INPUT_FILE = "../../Input/2022-day03";
 char CHUNK[256];
@@ -24,30 +26,47 @@ FILE* openFile(const char* INPUT_FILE)
     return fptr;
 }
 
-char* part1(FILE* fptr)
+int part1(FILE* fptr)
 {
-    if (fgets(CHUNK, sizeof(CHUNK), fptr) != NULL)
+    size_t sum = 0;
+    while (fgets(CHUNK, sizeof(CHUNK), fptr) != NULL)
     {
-        return CHUNK;
+        size_t chunk_len = strlen(CHUNK);
+        bool found       = false;
+
+        for (size_t i = 0; i < chunk_len / 2; ++i)
+        {
+            for (size_t j = chunk_len / 2; j < chunk_len; ++j)
+            {
+                if (found == true)
+                {
+                    break;
+                }
+
+                if (CHUNK[j] == CHUNK[i])
+                {
+                    if (CHUNK[j] >= 'a' && CHUNK[i] <= 'z')
+                    {
+                        sum += CHUNK[j] - 'a' + 1;
+                        found = true;
+                    }
+                    else if (CHUNK[j] >= 'A' && CHUNK[i] <= 'Z')
+                    {
+                        sum += CHUNK[j] - 'A' + 27;
+                        found = true;
+                    }
+                }
+            }
+        }
     }
-    else
-    {
-        return EOF_REACHED;
-    }
-    exit(COULD_NOT_READ_CHUNKS);
+    return sum;
 }
 
 int main(void)
 {
-
     FILE* fptr = openFile(INPUT_FILE);
 
-    while (part1(fptr) != EOF_REACHED)
-    {
-        // TODO(angelo): implement part 1 :/
-    }
+    printf("%d\n", part1(fptr));
 
     fclose(fptr);
-
-    return SUCCESS;
 };
